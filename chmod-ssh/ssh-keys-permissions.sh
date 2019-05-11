146e396115c649bcdd984e25cc0a236b4eb9ca67
Marco Matos <github@corp.mmatos.com>
2019-05-11 16:01:07 +0000

    fixing vars

--- chmod-ssh/ssh-keys-permissions.sh
+++ chmod-ssh/ssh-keys-permissions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # SSH keys hardening
 
-chmod go-w /home/$USER
-chmod 700 /home/$USER/.ssh
-chmod 644 /home/$USER/.ssh/authorized_keys
-chown $USER:$USER /home/$USER/.ssh/authorized_keys 
-chown $USER:$USER /home/$USER/.ssh
-usermod -d /home/$USER $USER
+chmod go-w $HOME
+chmod 700 $HOME/.ssh
+chmod 644 $HOME/.ssh/authorized_keys
+chown $USER:$USER $HOME/.ssh/authorized_keys 
+chown $USER:$USER $HOME/.ssh
+usermod -d $HOME $USER
 sudo service ssh restart
