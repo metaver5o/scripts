7907d59e04e9b86a6617e910babbaf1bac0fccd8
Marco Matos <github@corp.mmatos.com>
2019-05-20 06:46:33 +0000

    fixing identation

--- ansible-facts/facts.sh
+++ ansible-facts/facts.sh
@@ -1,7 +1,7 @@
 echo ">>> insert hostnames or group"; \
 	read hosts ; \
 echo ">>> insert the fact you want to retrieve:"; \
-read fact ; \
+	read fact ; \
 ansible -i hosts ${hosts} \
 	-m setup |cut -d\> -f 2 | \
 	jq -Mr .ansible_facts.${fact}
