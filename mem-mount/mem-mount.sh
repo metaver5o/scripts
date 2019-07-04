c75a754f233d44124fd47a1ce776a4b010ccb3f9
Marco Matos <marco_aurelio_matos@epam.com>
2019-07-04 14:07:22 +0200

    fixing var

--- mem-mount/mem-mount.sh
+++ mem-mount/mem-mount.sh
@@ -2,5 +2,5 @@ ram_tmp_bkp="/tmp/temp_extract" ;
 sudo umount "${ram_tmp_bkp}" ;
 mkdir -pv "${ram_tmp_bkp}" ;
 total_of_free_memory="$(free -m | grep -i "Mem:" | awk '{print $7}')" ;
-half_of_memory="$(expr ${total_of_free_memory} / 5)" ;
-sudo mount -t tmpfs -o size=${half_of_memory}M,nr_inodes=${half_of_memory}k,mode=700 tmpfs "${ram_tmp_bkp}" ;
+memory="$(expr ${total_of_free_memory} / 5)" ;
+sudo mount -t tmpfs -o size=${memory}M,nr_inodes=${memory}k,mode=700 tmpfs "${ram_tmp_bkp}" ;
